e25f95671986886ea01bc44d090b94c69bd2fcb2
tim72117 <tim72117@gmail.com>
2016-12-23 17:54:55 +0800

    master: index

---
+++ Foreacast.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import {
+    View,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    ListView,
+    Navigator
+} from 'react-native';
+
+class Foreacast extends Component {
+    constructor(props) {
+        super(props);
+        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {showText: true, listRows: ds.cloneWithRows([{}])};
+        setInterval(() => {
+            this.setState({ showText: !this.state.showText });
+        }, 1000);
+    }
+
+    _renderRow(product) {
+        return <Text style={styles.row}>{product.title}</Text>;
+    }
+
+    componentDidMount() {
+        fetch('http://104.199.155.0/products')
+        .then((response) => response.json())
+        .then((responseJSON) => {
+            this.setState({listRows: this.state.listRows.cloneWithRows(responseJSON.products)});
+            console.log(responseJSON);
+        });
+    }
+
+    _onPressButton() {
+        console.log(this.props.name);
+
+
+        this.setState({ showText: this.state.showText });
+        console.log("You tapped the button!");
+    }
+    render() {
+        const routes = [
+            {title: 'First Scene', index: 0},
+            {title: 'Second Scene', index: 1},
+        ];
+        let display = this.state.showText ? 'ggg' : ' ';
+        return (
+            // <Navigator
+            //     navigationBar={
+            //         <Navigator.NavigationBar
+            //             routeMapper={{
+            //                 LeftButton: (route, navigator, index, navState) =>
+            //                 {
+            //                     if (route.index === 0) {
+            //                     return null;
+            //                     } else {
+            //                     return (
+            //                         <TouchableHighlight onPress={() => navigator.pop()}>
+            //                         <Text>Back</Text>
+            //                         </TouchableHighlight>
+            //                     );
+            //                     }
+            //                 },
+            //                 RightButton: (route, navigator, index, navState) =>
+            //                 { return (<Text>Done</Text>); },
+            //                 Title: (route, navigator, index, navState) =>
+            //                 { return (<Text>Awesome Nav Bar</Text>); },
+            //             }}
+            //             style={{backgroundColor: 'gray'}}
+            //         />
+            //     }
+            //     initialRoute={routes[0]}
+            //     initialRouteStack={routes}
+            //     renderScene={(route, navigator) =>
+            //         <TouchableHighlight onPress={() => {
+            //             if (route.index === 0) {
+            //                 navigator.push(routes[1]);
+            //             } else {
+            //                 navigator.pop();
+            //             }
+            //         }}>
+            //             <Text>Hello {route.title}!</Text>
+            //         </TouchableHighlight>
+            //     }
+            //     style={{padding: 100, flex: 1}}
+            // />
+            <View style={styles.parent}>
+            <ListView style={styles.list} dataSource={this.state.listRows} renderRow={this._renderRow} />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    parent: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    list: {
+        flex: 1
+    },
+    row: {
+        flex: 1,
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: 'gray'
+    }
+});
+
+module.exports = Foreacast;
---
+++ index.android.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ActivityIndicator,
+  TextInput
+} from 'react-native';
+import Foreacast from './Foreacast';
+
+export default class myReact extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {zip: '', name: 'gg'};
+        //var ds = new ListView.DataSource({});
+    }
+    
+    _handleTextChange(event) {
+        this.setState({zip: event.nativeEvent.text});
+    }
+
+    render() {
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
+
+        console.log("You tapped the button!");
+        return (
+            <View style={styles.container}>
+                <Foreacast name={this.state.name} />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        flexDirection: 'row'
+    },
+});
+
+AppRegistry.registerComponent('myReact', () => myReact);
